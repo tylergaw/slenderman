7ef079543cdd9e545e0c003efb7842568ce723e1
Tyler Gaw <me@tylergaw.com>
2013-06-29 01:38:46 -0400

    Adding first round download site

---
+++ site/css/config.rb
@@ -0,0 +1,18 @@
+# This is a compass (sass/stylesheet) configuration file.
+#
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+
+css_dir = "."
+sass_dir = "./scss"
+
+# Output
+output_style = :compressed
+line_comments = false
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+preferred_syntax = :scss
+
+asset_cache_buster :none
